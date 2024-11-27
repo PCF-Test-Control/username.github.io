c8e3e6b52259e0cbf565f15ec194176b1ba41414
PCF-Test-Control <admin@CRM004135.onmicrosoft.com>
2024-11-27 12:44:38 -0800

    Add JS File

---
+++ EntityConfig.js
@@ -0,0 +1,66 @@
+{
+    "Entities": {
+        "Entity": [
+            {
+                "-name": "account",
+                "Field": [
+                    {
+                        "-SchemaName": "",
+                        "-DisplayName": "",
+                        "-FieldType": "",
+                        "-DisplayInGrid": "true",
+                        "-IsLookup": "false",
+                        "-ShowLink": "false",
+                        "-LinkEntitySchemaName": "",
+                        "-CustomLinkFunction": ""
+                    },
+                    {
+                        "-SchemaName": "",
+                        "-DisplayName": "",
+                        "-FieldType": "",
+                        "-DisplayInGrid": "true",
+                        "-IsLookup": "false",
+                        "-ShowLink": "false",
+                        "-LinkEntitySchemaName": "",
+                        "-CustomLinkFunction": ""
+                    }
+                ]
+            },
+            {
+                "-name": "contact",
+                "Field": [
+                    {
+                        "-SchemaName": "firstname",
+                        "-DisplayName": "First Name",
+                        "-FieldType": "string",
+                        "-DisplayInGrid": "true",
+                        "-IsLookup": "false",
+                        "-ShowLink": "false",
+                        "-LinkEntitySchemaName": "",
+                        "-CustomLinkFunction": ""
+                    },
+                    {
+                        "-SchemaName": "lastname",
+                        "-DisplayName": "Last Name",
+                        "-FieldType": "string",
+                        "-DisplayInGrid": "true",
+                        "-IsLookup": "false",
+                        "-ShowLink": "false",
+                        "-LinkEntitySchemaName": "",
+                        "-CustomLinkFunction": ""
+                    },
+                    {
+                        "-SchemaName": "emailaddress1",
+                        "-DisplayName": "Email Address",
+                        "-FieldType": "string",
+                        "-DisplayInGrid": "true",
+                        "-IsLookup": "false",
+                        "-ShowLink": "false",
+                        "-LinkEntitySchemaName": "",
+                        "-CustomLinkFunction": ""
+                    }
+                ]
+            }
+        ]
+    }
+}
